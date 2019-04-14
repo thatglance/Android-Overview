6d88e32d291d5bba09d0c9877d86abe4518a8a70
thatglance <11sashamak@gmail.com>
2019-04-14 23:26:41 +0300

    some fixes after code review

--- app/src/main/java/com/epam/training/androidoverview/MainActivity.kt
+++ app/src/main/java/com/epam/training/androidoverview/MainActivity.kt
@@ -8,36 +8,49 @@ import android.provider.MediaStore
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+/**
+ * This class loads picture from gallery into [imageView] at a press of a [chooseImageButton].
+ *
+ * @author Alexandra Makovskaya
+ */
 
-    val CHOOSE_IMAGE_REQUEST_CODE = 1
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         chooseImageButton.setOnClickListener {
-            val intent = Intent(Intent.ACTION_PICK)
-            intent.type = "image/*"
+            pickImageFromGallery()
+        }
+    }
 
-            if (intent.resolveActivity(packageManager) != null) {
-                startActivityForResult(intent, CHOOSE_IMAGE_REQUEST_CODE)
-            }
+    private fun pickImageFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK).apply {
+            type = INTENT_TYPE_IMAGE
+        }
 
+        intent.resolveActivity(packageManager)?.let {
+            startActivityForResult(intent, CHOOSE_IMAGE_REQUEST_CODE)
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == CHOOSE_IMAGE_REQUEST_CODE) {
-            if (resultCode == Activity.RESULT_OK) {
-                val imageURI = data?.data
-                val image = MediaStore.Images.Media.getBitmap(contentResolver, imageURI)
-                imageView.setImageBitmap(image)
-            } else {
-                Toast.makeText(this, "Other request", Toast.LENGTH_SHORT).show()
-            }
+        if (requestCode == CHOOSE_IMAGE_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val imageURI = data?.data
+            val image = MediaStore.Images.Media.getBitmap(contentResolver, imageURI)
+            imageView.setImageBitmap(image)
+        } else {
+            Toast.makeText(this, R.string.other_request_message, Toast.LENGTH_SHORT).show()
         }
+
     }
+
+    private companion object {
+        private const val CHOOSE_IMAGE_REQUEST_CODE = 1
+        private const val INTENT_TYPE_IMAGE = "image/*"
+    }
+
 }
